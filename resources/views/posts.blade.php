f69f13906dfd493f628cdd342b562f155ff35965
M. Usman Maulana <um59411@gmail.com>
2022-12-15 20:02:10 +0700

    memperbaiki judul halaman blog

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPU | Blo</title>
+    <title>WPU | Blog</title>
 </head>
 <body>
     <h1>Halaman Blog</h1>
