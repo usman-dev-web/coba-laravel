b2e981a8b5fd286c8fbbeae1f6c28b781408742d
M. Usman Maulana <um59411@gmail.com>
2022-12-16 13:30:53 +0700

    menambah jumbotron

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,12 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>WPU | {{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
 
     @include('partials.navbar')
       <div class="container mt-4">
           @yield('container')
+      </div>
+      @include('partials.jumbotron')
+      <div class="jumbotron">
+        @yield('jumbotron')
       </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
