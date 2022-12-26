afdb155c438313a91c1774670e96fafe32753f4b
M. Usman Maulana <um59411@gmail.com>
2022-12-26 20:45:51 +0700

    memperbaiki halaman single post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.main')
 
 @section('container')
-<article>
-        <h2>{{ $post->title }}</h2>
-        <p>By : <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-        {!! $post->body !!}
-    </article>
 
-    <a href="/posts">Back to posts</a>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h2 class="mb-3">{{ $post->title }}</h2>
+                <p>By : <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
+                <img src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+                <a href="/posts">Back to posts</a>
+            </div>
+        </div>
+    </div>
+
 @endsection 
