65234466cca04e2b32999951701dc5ff3e3d1fa8
M. Usman Maulana <um59411@gmail.com>
2022-12-26 19:16:06 +0700

    menambahkan link ke post category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -26,6 +26,7 @@
         @foreach ($posts->skip(1) as $post)
         <div class="col-md-4 mb-3">
           <div class="card">
+            <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
             <img src="https://source.unsplash.com/600x400/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
             <div class="card-body">
               <h5 class="card-title">{{ $post->title }}</h5>
