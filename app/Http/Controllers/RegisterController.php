7e22797ff253174e1bbe5d24af351f63cbed30fb
M. Usman Maulana <um59411@gmail.com>
2022-12-31 23:24:05 +0700

    user registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,4 +14,28 @@ class RegisterController extends Controller
             'active' => "login"
         ]);
     }
+
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // encrypt password
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        // dd('registration success');
+        // menyimpan ke database
+        User::create($validatedData);
+
+        // membuat notif ketika berhasil register
+        $request->session()->flash('success', 'Registration successfull! Please Login');
+
+        // redirect ke halaman login
+        return redirect('/login');
+    }
+
 }
