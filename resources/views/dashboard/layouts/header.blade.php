dc9709cef9b184baad466498dc7f3c177da9dc08
M. Usman Maulana <um59411@gmail.com>
2023-01-03 21:01:22 +0700

    membuat halaman my posts (resource controller)

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -8,7 +8,7 @@
       <div class="nav-item text-nowrap">
         <form action="/logout" method="post">
           @csrf
-          <button type="submit" class="nav-link px-3 bg-dark border-0"><i class="bi bi-box-arrow-in-left"></i> Logout</button>
+          <button type="submit" class="nav-link px-3 bg-dark border-0">Logout <span data-feather="log-out"></span></button>
         </form>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -65,6 +64,9 @@ Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+
 Route::get('/dashboard', function(){
         return view('dashboard.index');
 })->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
