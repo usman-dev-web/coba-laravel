9d868002ab89b9daa9c8e14606ad6c7df8afbc3c
M. Usman Maulana <um59411@gmail.com>
2023-01-08 13:16:53 +0700

    membuat combo box category

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -15,6 +15,14 @@
               <label for="slug" class="form-label">Slug</label>
               <input type="text" class="form-control" id="slug" name="slug" disabled readonly>
             </div>
+            <div class="mb-3">
+              <label for="category" class="form-label">Category</label>
+              <select class="form-select" name="category_id">
+                @foreach ($categories as $category)
+                  <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+              </select>
+            </div>
 
             <button type="submit" class="btn btn-primary">Create Post</button>
           </form>
