afc04c34eb1c2dded7da034d78eba7149b32dc89
M. Usman Maulana <um59411@gmail.com>
2023-01-08 20:16:00 +0700

    menampilkan pesan error

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -9,11 +9,21 @@
             @csrf
             <div class="mb-3">
               <label for="title" class="form-label">Title</label>
-              <input type="text" class="form-control" id="title" name="title">
+              <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus>
+              @error('title')
+               <div class="invalid-feedback">
+                {{ $message }}  
+              </div>   
+              @enderror
             </div>
             <div class="mb-3">
               <label for="slug" class="form-label">Slug</label>
-              <input type="text" class="form-control" id="slug" name="slug">
+              <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required>
+              @error('title')
+              <div class="invalid-feedback">
+               {{ $message }}  
+             </div>   
+             @enderror
             </div>
             <div class="mb-3">
               <label for="category" class="form-label">Category</label>
@@ -25,6 +35,9 @@
             </div>
             <div class="mb-3">
               <label for="body" class="form-label">Body</label>
+              @error('body')
+               <p class="text-danger"> {{ $message }} </p>
+              @enderror
                 <input id="body" type="hidden" name="body">
                 <trix-editor input="body"></trix-editor>
               </select>
