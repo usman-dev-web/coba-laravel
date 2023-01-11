f4ef5b552d8e5075e8010d266f23723a56249d4d
M. Usman Maulana <um59411@gmail.com>
2023-01-11 21:33:44 +0700

    menampilkan image pada view post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -13,7 +13,14 @@
                 <button class="btn btn-danger" onclick="return confirm('are u sure?')"><span data-feather="x-circle"></span> Delete Post</button>
               </form>
 
-            <img src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+             @if ($post->image)
+             <div style="max-height: 350px; overflow:hidden;">
+                 <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+             </div>
+            @else
+                <img src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+            @endif 
+
 
             <article class="my-3 fs-5">
                 {!! $post->body !!}
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,8 +7,14 @@
             <div class="col-md-8 bg">
                 <h2 class="mb-3">{{ $post->title }}</h2>
                 <p>By : <a href="/posts?author={{ $post->author->username }}">{{ $post->author->name }}</a> <a href="/posts?category={{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-
-                <img src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+                
+                @if ($post->image)
+                <div style="max-height: 350px; overflow:hidden;">
+                    <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid">
+                </div>
+               @else
+                    <img src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+               @endif 
 
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
